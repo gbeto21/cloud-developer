e0fe5b4d0e6794e3a561d58a57099d30674656f7
Alberto González <albertoglezb@hotmail.com>
2021-10-14 21:58:22 -0500

    The proyect was deployed to Beanstalk.

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,26 @@
+require('dotenv').config()
+
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "operatorsAliases": false,
+    "aws_region": process.env.AWS_REGI,
+    "aws_profile": process.env.AWS_PROF,
+    "aws_media_bucket": process.env.AWS_BUCK
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "aws_region": process.env.AWS_REGI,
+    "aws_profile": process.env.AWS_PROF,
+    "aws_media_bucket": process.env.AWS_BUCK
   }
 }
