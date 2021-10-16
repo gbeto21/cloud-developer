4bdc6a17e9d00ad9923d64aed1955e5196ab3a2c
Alberto González <albertoglezb@hotmail.com>
2021-10-16 14:33:15 -0500

    The end point to changue the image was implemented.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,46 +1,53 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
+
+  //Images to delete after were sent
+  const imagesDelete: string[] = []
 
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req, res) => {
+    try {
+      const url = req.query.image_url
 
-  /**************************************************************************** */
+      if (url) {
+        const image = await filterImageFromURL(url)
+        const split = image.split('/')
+        const imageName = split[split.length - 1]
+        const urlImageDelete = `${__dirname}/util/tmp/${imageName}`
+        if (imagesDelete.length) {
+          await deleteLocalFiles(imagesDelete)
+        }
+        imagesDelete.push(urlImageDelete)
+        res.sendFile(image);
+      }
+      else { 
+        res.status(400).send('No image specified.')
+      }
+    } catch (error) {
+      console.error(error);
+      res.send("Server error").sendStatus(500)
+    }
+  })
 
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
